c750c0c3d8ac30d9c07b2998b93b8db16311db23
Milan Vlahovic <flexa@flexa.cz>
2013-05-22 16:51:43 +0200

    tic tac toe

--- tic_tac_toe/js/tic_tac_toe_plan.class.js
+++ tic_tac_toe/js/tic_tac_toe_plan.class.js
@@ -1,14 +1,37 @@
 function TicTacToePlan(){
-	var id=null;
+	var element_id=null;
 	var plan=null;
 	var cols=20;
 	var rows=20;
+	var active_player=1;
+	var player_1=new Array();
+	player_1['symbol']='&times;';
+	var player_2=new Array();
+	player_2['symbol']='&cir;';
+	var players=new Array();
+	players[1]=player_1;
+	players[2]=player_2;
+	var active_cell=null;
+	var active_row=null;
+	var active_col=null;
+	var active_hover=null;
+	var last_play=null;
 
+	/**
+	 * spustit po nacteni okna, aby se hra vytvorila
+	 * @param {type} id
+	 * @returns {undefined}
+	 */
 	this.Init=function(id){
 		plan=$('#'+id);
-		this.Create().BindActions();
+		element_id=id;
+		this.Create().BindActions(this);
 	};
 
+	/**
+	 * vytvori bunku
+	 * @returns {TicTacToePlan}
+	 */
 	this.Create=function(){
 		var act_row=null;
 		for (var row=1;row<=rows;row++){
@@ -21,18 +44,74 @@ function TicTacToePlan(){
 		return this;
 	};
 
-	this.BindActions=function(){
-		$('#'+id+' .cell').each(function(){
-			$(this).click(function(){
-				this.Click($(this));
-			});
+	/**
+	 * povesi vsechny potrebne udalosti
+	 * @param {type} self
+	 * @returns {TicTacToePlan}
+	 */
+	this.BindActions=function(self){
+		$('#'+element_id+' .cell').each(function(){
+			$(this)
+				.click(function(){
+					self.Click($(this));
+				})
+				.mouseenter(function(){
+					active_hover=$(this);
+					$(this).addClass('hover');
+				})
+				.mouseleave(function(){
+					active_hover.removeClass('hover');
+				});
 		});
+		return this;
 	};
 
+	/**
+	 * provede klik na bunku
+	 * @param {type} cell
+	 * @returns {TicTacToePlan}
+	 */
 	this.Click=function(cell){
-		console.log('click...');
-//		var col=cell.attr('rel');
-//		var row=cell.parent('div.row').attr('rel');
-//		console.log('row: ' + row + '; col: ' + col);
+		this.SetCell(cell).Play();
+		return this;
+	};
+
+	/**
+	 * zkusi provest tah na aktualnim poli
+	 * @returns {TicTacToePlan}
+	 */
+	this.Play=function(){
+		if(!active_cell.html()){
+			if(last_play && last_play.length){
+				last_play.removeClass('last-play');
+			}
+			active_cell.html(players[active_player]['symbol']).addClass('last-play');
+			last_play=active_cell;
+			this.SwitchPlayer();
+		}
+		return this;
+	};
+
+	/**
+	 * nastaveni aktualni bunky
+	 * @param {type} cell
+	 * @returns {TicTacToePlan}
+	 */
+	this.SetCell=function(cell){
+		var col=cell.attr('rel');
+		var row=cell.parent('div.row').attr('rel');
+		active_cell=cell;
+		active_row=row;
+		active_col=col;
+		return this;
+	};
+
+	/**
+	 * prepne na druheho hrace
+	 * @returns {TicTacToePlan}
+	 */
+	this.SwitchPlayer=function(){
+		active_player=(active_player===1 ? 2 : 1);
+		return this;
 	};
 }
