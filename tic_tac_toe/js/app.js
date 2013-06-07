8f0cc77e58490a645398abc6187d8a3a2946b03e
Milan Vlahovic <flexa@flexa.cz>
2013-06-07 13:23:37 +0200

    poznamky k tic_tac_toe

--- tic_tac_toe/js/app.js
+++ tic_tac_toe/js/app.js
@@ -1,3 +1,8 @@
+/**
+ * @todo vytvorit jeste jednen objekt tic_tac_toe a do nej prendat cast logiky z plan.class.js
+ * @todo player musi vracet pole odehranych tahu v poradi id (ne asociativni, ale normalni)
+ * @todo game nemusi prochazet tolikrat pole tahu, staci jednou na vytvoreni skupin
+ */
 $(window).ready(function(){
 	var player_1=new Player(1,'&cir;','Milan',$('div.debug_info.player_1'));
 	var player_2=new Player(2,'&times;','Comp.',$('.debug_info.player_2'),true);
