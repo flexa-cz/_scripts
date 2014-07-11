f486b94f1b80778eeaf362c566bf1d87db26e187
Milan Vlahovič <flexa@flexa.cz>
2014-07-11 08:36:49 +0200

    js games
    snake completed, create dirs for tetris

--- tic_tac_toe/js/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * @todo vytvorit jeste jednen objekt tic_tac_toe a do nej prendat cast logiky z plan.class.js
- * @todo player musi vracet pole odehranych tahu v poradi id (ne asociativni, ale normalni)
- * @todo game nemusi prochazet tolikrat pole tahu, staci jednou na vytvoreni skupin
- */
-$(window).ready(function(){
-	var tic_tac_toe=new TicTacToe();
-	tic_tac_toe.Init(tic_tac_toe,'tic_tac_toe',20,20);
-});
